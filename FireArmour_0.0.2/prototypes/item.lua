4235048c2037338637b87edcc662fd688749084b
BennehR <ben.c.rendall@gmail.com>
2018-01-11 16:57:49 +0000

    Changed item.lua to generate a new item rather than pulling from the raw table
    Added a modified icon
    
    Added a recipe.lua that dictates to craft the item in a chem plant

--- FireArmour_0.0.2/prototypes/item.lua
+++ FireArmour_0.0.2/prototypes/item.lua
@@ -1,43 +1,41 @@
 --item.lua
 
-local fireArmour = table.deepcopy(data.raw.armor["heavy-armor"])
-
-fireArmour.name = "Fire-Armour"
-fireArmour.icons= {
+data:extend(
+{
     {
-        icon=fireArmour.icon,
-        tint={r=1,g=0,b=0,a=0.3}
-    },
+        type = "armor",
+        name = "Fire-Armour",
+        icon = "__FireArmour__/graphics/icons/fire-armour.png",
+        tint = {r=1,g=0,b=0,a=0.3},
+        icon_size = 32,
+        flags = {"goes-to-main-inventory"},
+        resistances =
+        {
+            {
+                type = "physical",
+                decrease = 6,
+                percent = 30
+            },
+            {
+                type = "explosion",
+                decrease = 20,
+                percent = 30
+            },
+            {
+                type = "acid",
+                decrease = 3,
+                percent = 30
+            },
+            {
+                type = "fire",
+                decrease = 0,
+                percent = 100
+            }
+        },
+        durability = 5000,
+        subgroup = "armor",
+        order = "b-a[heavy-armor]",
+        stack_size = 10
+    }
 }
-
-fireArmour.resistances = {
-    {
-        type = "physical",
-        decrease = 6,
-        percent = 10,
-    },
-    {
-        type = "explosion",
-        decrease = 10,
-        percent = 30
-    },
-    {
-        type = "acid",
-        decrease = 5,
-        percent = 30
-    },
-    {
-        type = "fire",
-        decrease = 0,
-        percent = 100
-    },
-}
-
-fireArmour.order = "f[fire-armour]"
-
-local recipe = table.deepcopy(data.raw.recipe["heavy-armor"])
-recipe.enabled = true;
-recipe.ingredients = {{"copper-plate",200},{"steel-plate",50}}
-recipe.result = "Fire-Armour"
-
-data:extend{fireArmour,recipe}
+)
---
+++ FireArmour_0.0.2/prototypes/recipe.lua
@@ -0,0 +1,15 @@
+data:extend(
+{
+    {
+        type = "recipe",
+        category = "chemistry",
+        name = "Fire-Armour",
+        ingredients = {
+            {"heavy-armor",1},
+            {"coal",1}
+        },
+        result = "Fire-Armour",
+        enabled = true
+    }
+}
+)
